29a375b2f1712cb20e41d24e6644c32906bafe39
Wasif Al Mahmud <wamvmaw@gmail.com>
2020-07-10 01:47:07 +0600

    Project initialized

---
+++ src/test/java/com/wsf/workflow/WorkflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wsf.workflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
